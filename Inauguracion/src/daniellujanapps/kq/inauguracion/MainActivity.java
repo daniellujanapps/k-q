05f9d8f0da3a5d1be9ea6af2180cbe86fa171566
daniellujan <daniellujanapps@gmail.com>
2014-11-25 11:19:47 -0600

    - cameraView & OpenCV

--- Inauguracion/src/daniellujanapps/kq/inauguracion/MainActivity.java
+++ Inauguracion/src/daniellujanapps/kq/inauguracion/MainActivity.java
@@ -1,9 +1,9 @@
 package daniellujanapps.kq.inauguracion;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 
 @SuppressWarnings("unused")
 public class MainActivity extends Activity {
@@ -12,5 +12,29 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void ninoBtnClick(View v){
+//    	CharSequence text = "Niñooo!";
+//    	int duration = Toast.LENGTH_SHORT;
+//
+//    	Toast toast = Toast.makeText(getApplicationContext(), text, duration);
+//    	toast.show();
+    	Intent intent = new Intent(this, FotoActivity.class);
+    	String message = "nino";
+    	intent.putExtra("message", message);
+    	startActivity(intent);
+    }
+    
+    public void ninaBtnClick(View v){
+//    	CharSequence text = "Niñaaaa!";
+//    	int duration = Toast.LENGTH_SHORT;
+//
+//    	Toast toast = Toast.makeText(getApplicationContext(), text, duration);
+//    	toast.show();
+    	Intent intent = new Intent(this, FotoActivity.class);
+    	String message = "nina";
+    	intent.putExtra("message", message);
+    	startActivity(intent);
     }
 }
