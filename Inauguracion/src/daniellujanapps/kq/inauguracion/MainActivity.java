8e8adb198f704b52870af56cd1a5015073b32a0c
daniellujan <daniellujanapps@gmail.com>
2014-11-24 18:04:31 -0600

    -init

---
+++ Inauguracion/src/daniellujanapps/kq/inauguracion/MainActivity.java
@@ -0,0 +1,16 @@
+package daniellujanapps.kq.inauguracion;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+@SuppressWarnings("unused")
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
